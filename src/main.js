107e61a12d2f5436f8695749fca01058ff138465
Chiquitta <linyuting@MacBook-Pro.local>
2019-10-30 11:20:12 +0800

    add ga

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(VueAnalytics, {
+  id: 'UA-151210580-1',
+  checkDuplicatedScript: true
+})
 
 Vue.config.productionTip = false
 
