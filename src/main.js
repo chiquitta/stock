52fefcfd42828a18664fcb8817ceaa18691311ea
Chiquitta <linyuting@MacBook-Pro.local>
2019-10-30 11:40:38 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import VueAnalytics from 'vue-analytics'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(VueAnalytics, {
-  id: 'UA-151210580-1',
-  checkDuplicatedScript: true
-})
-
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
+Vue.use(VueAnalytics, {
+  id: 'UA-151210580-1',
+  checkDuplicatedScript: true
+})
