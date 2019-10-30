b88830d4242f09e9bc73807dd4c0d7dd4a319f21
Chiquitta <linyuting@MacBook-Pro.local>
2019-10-30 12:04:52 +0800

    uninstall vue-analytic

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-import VueAnalytics from 'vue-analytics'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-Vue.use(VueAnalytics, {
-  id: 'UA-151210580-1',
-  disableScriptLoader: true
-})
 
 new Vue({
   render: h => h(App),
