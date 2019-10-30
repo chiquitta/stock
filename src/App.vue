f4d4105773e1a7827339cdf225b695dc48da8178
Chiquitta Lin <chiquitta@brandma.co>
2019-10-30 20:30:57 +0800

    update sell section

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <div id="app">
     <b-container>
       <b-row class="py-4">
-        <b-col cols="12" class="pb-3">
-          <!-- <h1>
+        <b-col cols="12" class="pb-5">
+          <h2 class="font-weight-light text-center">
             台股買賣獲利線上試算
-          </h1> -->
+          </h2>
         </b-col>
-        <b-col cols="12" md="6">
+        <b-col cols="12">
           <b-row>
-            <b-col>
+            <b-col cols="6" md="3" order="1" order-sm="1">
               <b-form-group
                 label="買入成交價"
                 class="text-black-50"
@@ -17,51 +17,95 @@
                 <b-form-input v-model="buy" type="number" size="lg" placeholder="請輸入" />
               </b-form-group>
             </b-col>
-            <b-col>
+            <b-col cols="6" md="3" order="3" order-sm="2">
               <b-form-group
                 label="買入股數"
                 class="text-black-50"
-                description="*一張為 1000 股"
+                description="一張為 1000 股"
               >
                 <b-form-input v-model="buyNum" type="number" size="lg" placeholder="請輸入" />
               </b-form-group>
             </b-col>
+            <b-col cols="6" md="3" order="2" order-sm="3">
+              <b-form-group
+                label="賣出成交價"
+                class="text-black-50"
+              >
+                <b-form-input v-model="sell" type="number" size="lg" placeholder="請輸入" />
+              </b-form-group>
+            </b-col>
+            <b-col cols="6" md="3" order="4" order-sm="4">
+              <b-form-group
+                label="賣出股數"
+                class="text-black-50"
+                description="一張為 1000 股"
+              >
+                <b-form-input v-model="sellNum" type="number" size="lg" placeholder="請輸入" />
+              </b-form-group>
+            </b-col>
+            <b-col cols="6" order="5" order-sm="5">
+              <label class="text-black-50">
+                券商下單折扣
+                <b-link class="small" @click="manualFeeA = !manualFeeA">
+                  手動輸入
+                </b-link>
+              </label>
+              <b-form-select v-if="!manualFeeA" v-model="sellFee" :options="fees" size="lg" />
+              <b-input-group v-else append="折">
+                <b-form-input v-model="sellFee" type="number" size="lg" placeholder="請輸入" />
+              </b-input-group>
+            </b-col>
+            <b-col cols="6" order="6" order-sm="6">
+              <label class="text-black-50">
+                券商下單折扣
+                <b-link class="small" @click="manualFeeB = !manualFeeB">
+                  手動輸入
+                </b-link>
+              </label>
+              <b-form-select v-if="!manualFeeB" v-model="buyFee" :options="fees" size="lg" />
+              <b-input-group v-else append="折">
+                <b-form-input v-model="buyFee" type="number" size="lg" placeholder="請輸入" />
+              </b-input-group>
+            </b-col>
           </b-row>
-          <b-form-group
-            label="券商下單折扣"
-            class="text-black-50"
-            description="可自行輸入折數"
-          >
-            <b-form-select v-model="fee" :options="fees" size="lg" />
-            或
-            <b-input-group append="折">
-              <b-form-input v-model="fee" type="number" size="lg" placeholder="請輸入" />
-            </b-input-group>
-          </b-form-group>
-          <b-row class="pt-4">
-            <b-col class="text-right">
+          <b-row class="pt-5">
+            <b-col cols="4" class="text-right">
               <span class="text-black-50">買入價格</span>
               <p class="lead">
                 ${{ buyPrice.toLocaleString() }}
               </p>
             </b-col>
-            <b-col class="text-right">
+            <b-col cols="4" class="text-right">
               <span class="text-black-50">買入手續費</span>
               <p class="lead">
-                ${{ buyFee.toLocaleString() }}
+                ${{ bfee.toLocaleString() }}
               </p>
             </b-col>
-            <b-col class="text-right">
+            <b-col cols="4" class="text-right">
             <span class="text-black-50">買入總價格</span>
             <p class="lead">
               ${{ buyTotal.toLocaleString() }}
             </p>
             </b-col>
+            <b-col class="text-right">
+              <span class="text-black-50">賣出價格</span>
+              <p class="lead">
+                ${{ sellPrice.toLocaleString() }}
+              </p>
+            </b-col>
+            <b-col class="text-right">
+              <span class="text-black-50">賣出手續費</span>
+              <p class="lead">
+                ${{ sfee.toLocaleString() }}
+              </p>
+            </b-col>
+            <b-col class="text-right">
+            <span class="text-black-50">賣出總價格</span>
+            <p class="lead">
+              ${{ sellTotal.toLocaleString() }}
+            </p>
+            </b-col>
           </b-row>
-        </b-col>
-        <b-col>
-          <!-- <b-form-input v-model="sell" placeholder="" />
-          <b-form-input v-model="sellNum" placeholder="請輸入" /> -->
         </b-col>
       </b-row>
     </b-container>
@@ -83,11 +127,14 @@ export default {
     return {
       buy: 40,
       buyNum: 1000,
-      sell: 0,
+      sell: 42,
       sellNum: 1000,
+      manualFeeA: false,
+      manualFeeB: false,
       brokerFee: null,
       feePercentage: 0.001425,
-      fee: 6,
+      buyFee: 6,
+      sellFee: 6,
       fees: this.range(0, 10, 0.5).map(val => {
         let text = `${val}折`
         text = val === 10 ? '無折扣' : text
@@ -100,13 +147,25 @@ export default {
     buyPrice() {
       return this.buy * this.buyNum
     },
-    buyFee() {
-      const brokerFee = this.buy * this.buyNum * this.feePercentage * this.fee * 0.1
+    bfee() {
+      const brokerFee = this.buyPrice * this.feePercentage * this.buyFee * 0.1
+      if(brokerFee === 0) return 0
       return Math.round(brokerFee < 20 ? 20 : brokerFee)
     },
     buyTotal() {
-      return Math.round(this.buyPrice + this.buyFee)
-    }
+      return Math.round(this.buyPrice + this.bfee)
+    },
+    sellPrice() {
+      return this.sell * this.sellNum
+    },
+    sfee() {
+      const brokerFee = this.sellPrice * this.feePercentage * this.sellFee * 0.1
+      if(brokerFee === 0) return 0
+      return Math.round(brokerFee < 20 ? 20 : brokerFee)  
+    },
+    sellTotal() {
+      return Math.round(this.sellPrice + this.sfee)
+    },
   },
   methods: {
     range(start, end, step = 1) {
@@ -118,6 +177,9 @@ export default {
 </script>
 
 <style>
+.v-icon {
+  width: 18px;
+}
 /* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import feather from 'vue-icon'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(feather, {
+  name: 'v-icon',
+  props: {
+    baseClass: {
+        type: String,
+        default: 'v-icon'
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
